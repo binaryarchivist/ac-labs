ed42ce4eed17a26cfca042406d0c65da6b3cb073
Corneliu Nastas <cornel.nastas@corlab.tech>
2023-04-10 14:41:36 +0300

    lab4: sum of two numbers in asm

--- sums.asm
+++ sums.asm
@@ -6,7 +6,12 @@
 section .data
     prompt db "Enter a value: ", 0
     .len   equ $ - prompt
-    printSum db  "Sum: %d", 0
+
+    message db "Sum: "
+    msglen equ $ - message
+
+    zero equ '0'
+
     SYSCALL_WRITE equ       0x2000004
     SYSCALL_EXIT  equ       0x2000001
     SYSCALL_READ  equ       0x2000003
@@ -14,32 +19,61 @@ section .data
 section .bss
     firstNum resb 32
     secondNum resb 32
-    sum       resb 32
+    sum       resq 64
 
 
 section .text
     global _start
 
 _start:
-        mov       rax, SYSCALL_WRITE          ; system call for write
-        mov       rdi, 1                  ; file handle 1 is stdout
-        mov       rsi, prompt             ; address the string message
-        mov       rdx, prompt.len         ; number of bytes
-        syscall                           ; invoke operating system to do the write
+    mov       rax, SYSCALL_WRITE          ; system call for write
+    mov       rdi, 1                  ; file handle 1 is stdout
+    mov       rsi, prompt             ; address the string message
+    mov       rdx, prompt.len         ; number of bytes
+    syscall                           ; invoke operating system to do the write
 
-        mov     rax, SYSCALL_READ
-        mov     rdi, 0
-        mov     rsi, firstNum
-        mov     rdx, 32
-        syscall
+    mov     rax, SYSCALL_READ
+    mov     rdi, 0
+    mov     rsi, firstNum
+    mov     rdx, 32
+    syscall
 
-        mov       rax, SYSCALL_WRITE       ; system call for write
-        mov       rdi, 1                  ; file handle 1 is stdout
-        mov       rsi, prompt             ; address the string message
-        mov       rdx, prompt.len         ; number of bytes
-        syscall                           ; invoke operating system to do the write
+    mov       rax, SYSCALL_WRITE       ; system call for write
+    mov       rdi, 1                  ; file handle 1 is stdout
+    mov       rsi, prompt             ; address the string message
+    mov       rdx, prompt.len         ; number of bytes
+    syscall                           ; invoke operating system to do the write
 
-        mov       rax, SYSCALL_EXIT          ; system call for exit
-        mov       rdi, 0                  ; exit code 0
-        syscall                           ; invoke operating system to exit
-        ret
+    mov     rax, SYSCALL_READ
+    mov     rdi, 0
+    mov     rsi, secondNum
+    mov     rdx, 32
+    syscall
+
+    mov r8, [rel firstNum]
+    sub r8, '0'
+
+    mov r9, [rel secondNum]
+    sub r9, '0'
+
+    add r8, r9
+    add r8, '0'
+
+    mov [rel sum], r8
+
+    mov rax, SYSCALL_WRITE
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, msglen
+    syscall
+
+    mov rax, SYSCALL_WRITE
+    mov rdi, 1
+    mov rsi, sum
+    mov rdx, 64
+    syscall
+
+    mov       rax, SYSCALL_EXIT          ; system call for exit
+    mov       rdi, 0                  ; exit code 0
+    syscall                           ; invoke operating system to exit
+    ret
